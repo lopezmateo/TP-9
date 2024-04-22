88d3e02c74e955240de7e0e9f022cdc67a2262b6
lopezmateo <163901238+lopezmateo@users.noreply.github.com>
2024-04-22 13:37:00 -0300

    Add files via upload

---
+++ Untitled1.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+int main (){
+
+ string nombre;
+    float nota1, nota2, nota3, promedio;
+
+
+    cout << "Ingrese el nombre del alumno: ";
+    getline(cin, nombre);
+
+
+    cout << "Ingrese la primera nota: ";
+    cin >> nota1;
+
+    cout << "Ingrese la segunda nota: ";
+    cin >> nota2;
+
+    cout << "Ingrese la tercera nota: ";
+    cin >> nota3;
+
+
+    promedio = (nota1 + nota2 + nota3) / 3.0;
+
+
+    if (promedio >= 8 && promedio <= 10) {
+        cout << "Excelente " <<endl;
+    } else if (promedio >= 7) {
+        cout << "Aprobo" <<endl;
+    } else if(promedio<7) {
+        cout <<"Desaprobo" <<endl;
+    }
+
+  return 0;
+}
